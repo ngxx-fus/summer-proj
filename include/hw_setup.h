1628c3b238bac3843f41ddeca0bbe34ee451096c
ngxx-fus <msnp@outlook.com.vn>
2025-08-11 21:34:28 +0700

    changed

---
+++ include/hw_setup.h
@@ -0,0 +1,12 @@
+#ifndef __HW_SETUP_H__
+#define __HW_SETUP_H__
+
+#define PULLUP_BUTTON   GPIO_NUM_34
+#define DTH11_OUT       GPIO_NUM_17
+#define VAR_RES         GPIO_NUM_35
+#define OLED_SCL        GPIO_NUM_21
+#define OLED_SDA        GPIO_NUM_22
+#define YELLOW_LED      GPIO_NUM_19
+#define GREEN_LED       GPIO_NUM_18
+
+#endif
